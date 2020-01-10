6839c8e0b2bacd8d85ea2b783abb2e4119b39eff
nimeijian <nimeijian@yunzhangfang.com>
2020-01-10 21:38:26 +0800

    优化

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-name="172.24.0.28:1180/yzf-apollo/apollo-all"
+name="apollo-all"
 tag=1.5.1
 
-docker build -t ${name}:${tag} .
+docker build --build-arg maven_repo=/home/nimeijian/software/maven/repo -t ${name}:${tag} .
 
 oldImage=$(docker images | grep ${name} | grep -v ${tag} | awk -vOFS=":" '{ print $1,$2 }')
 if [[ -n ${oldImage} ]];then
